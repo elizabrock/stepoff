5cd9ddc25cd4de54d9318bfdaab6a2c4e146fe58
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-06-03 10:52:30 -0500

    updated schema after migration of lap model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602194430) do
+ActiveRecord::Schema.define(version: 20150603153808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "laps", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "track_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "laps", ["track_id"], name: "index_laps_on_track_id", using: :btree
+  add_index "laps", ["user_id"], name: "index_laps_on_user_id", using: :btree
 
   create_table "tracks", force: :cascade do |t|
     t.string   "name"
@@ -33,4 +43,6 @@ ActiveRecord::Schema.define(version: 20150602194430) do
     t.string   "crypted_password"
   end
 
+  add_foreign_key "laps", "tracks"
+  add_foreign_key "laps", "users"
 end
